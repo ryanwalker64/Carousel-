1a33f16df75b559d8ca75a8f0d4f612e05bc527c
ryanwalker64 <ryan.walkerz46@gmail.com>
2022-07-09 18:15:13 +1200

    complete

--- script.js
+++ script.js
@@ -1,14 +1,40 @@
 const sliderButtons = document.querySelectorAll('[data-slider-button]'); 
 const slideContainer = document.querySelector('[data-slide-container]'); 
+const slidesArr = document.querySelectorAll('[data-slide]');
+const slideDotsContainer = document.querySelector('[data-slide-dots]');
+
+function sliderDots() {
+    const sliderArray = [...slidesArr];
+    const dotsHtml = sliderArray.map(slide => {
+        return `<div class='dot' data-dotid='${slide.dataset.slideid}' data-dot='${slide.dataset.slide === 'active' ? 'active' : ''}'></div>`
+    }).join('')
+
+    slideDotsContainer.innerHTML = dotsHtml;
+    const dotBtns = document.querySelectorAll('[data-dot]');
+    dotBtns.forEach(dot => {
+        dot.addEventListener('click', (e) => {
+            const idDot = e.currentTarget.dataset.dotid;
+            const activeSlide = document.querySelector('[data-slide="active"]')
+            
+            activeSlide.dataset.slide = ''
+            for (let i = 0; i < slidesArr.length; i++){
+                if (slidesArr[i].dataset.slideid === idDot) {
+                    slidesArr[i].dataset.slide = 'active';
+                }
+            }
+            sliderDots()
+        })
+    })
+}
+
+sliderDots()
+
+
 
 
 sliderButtons.forEach(btn => {
-    btn.addEventListener('click', (e) => {
-       
+    btn.addEventListener('click', (e) => { 
         handleSlideSwitch(e.currentTarget.dataset.sliderButton)
-       
-       
-    
         
     })
 })
@@ -25,6 +51,7 @@ function handleSlideSwitch(direction = 'next') {
     }
 
     activeSlide.dataset.slide = ''
+  
 
     if(direction === 'next') {
         if (activeSlideIndex == slidesArr.length - 1) {
@@ -40,8 +67,17 @@ function handleSlideSwitch(direction = 'next') {
             slidesArr[activeSlideIndex - 1].dataset.slide = 'active';
         }
     }
+    sliderDots()
 
 }
+
+
+
+
+window.setInterval(() => {
+    handleSlideSwitch();
+}, 10000);
+
 
 
 
